4b6f50595184ef4482e9617ad5e10699059b7863
sabuthess <dilan.developer22@gmail.com>
2026-08-14 23:55:20 -0500

    remove light and dark binaries .bat

--- src-tauri/binaries/dark_mode-x86_64-pc-windows-msvc.bat
+++
@@ -1,6 +0,0 @@
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v SystemUsesLightTheme /t REG_DWORD /d 0 /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v AppsUseLightTheme /t REG_DWORD /d 0 /f
-
-taskkill /f /im explorer.exe
-
-start explorer.exe
--- src-tauri/binaries/ligh_mode-x86_64-pc-windows-msvc.bat
+++
@@ -1,6 +0,0 @@
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v SystemUsesLightTheme /t REG_DWORD /d 1 /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v AppsUseLightTheme /t REG_DWORD /d 1 /f
-
-taskkill /f /im explorer.exe
-
-start explorer.exe
